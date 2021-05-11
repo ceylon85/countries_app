3cb43576833627e20f53d43c68d1cab6ad91a6b0
ceylon85 <canleez123@gmail.com>
2021-05-11 18:48:49 +0900

    Countries_App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Details from "./components/pages/Details";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home mode={mode} />
+          </Route>
+          <Route paht="/">
+            <Details mode={mode} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+import "./SearchBar.css";
+
+function SearchBar(props) {
+  const inputHandler = (e) => {
+    props.filterByInput(e.target.value);
+  };
+  return (
+    <div className={`relative searchbar-component searchbar-${props.mode}`}>
+      <input
+        type="search"
+        placeholder={props.placeholder}
+        className={`searchbar ${props.className}`}
+        onChange={inputHandler}
+      />
+      <FontAwesomeIcon icon={faSearch} />
+    </div>
+  );
+}
+
+export default SearchBar;
